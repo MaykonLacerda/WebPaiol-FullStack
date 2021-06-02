537934a8ad3dc278573442b5675e4eb63267a521
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-06-02 17:01:53 -0300

    add row user

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,12 @@
 import { Router } from 'express';
 
+import { UsersController } from './accounts/controllers/UsersController'
+
 const routes = Router();
+
+const usersController = new UsersController()
+
+routes.post('/user', usersController.create)
+routes.get('/login', usersController.showUsers)
 
 export { routes };
