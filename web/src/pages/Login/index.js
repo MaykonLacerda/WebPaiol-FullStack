397113d6438bfe6ffaa9da45251fb6ea9e9010a7
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-06-03 18:50:52 -0300

    login validation

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import StoreContext from '../../components/Store/Context';
 
 import api from '../../services/api';
 
-import SecondButton from '../../components/Buttons/SecondButton/SecondButton'
+import SecondButton from '../../components/Buttons/SecondButton/SecondButton';
 
-import './styles.css'
+import './styles.css';
 
 function initialState() {
   return { banca: '', password: '' };
 }
 
+function login({ banca, password }) {
+  if(banca === 'admin' && password ==='admin'){
+    return { token: '12345' };
+  } else {
+    return { error: "Usuário ou senha inválido" };
+  }
+
+}
+
 export const Login = () => {
   const [values, setValues] = useState(initialState);
+  const { setToken } = useContext(StoreContext);
+  const history = useHistory();
 
   function onChange(event) {
     const { value, name } = event.target;
@@ -23,20 +35,33 @@ export const Login = () => {
     })
   }
 
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const { token } = login(values);
+
+    if(token) {
+      setToken(token);
+      return history.push('/dashboard');
+    }
+
+    setValues(initialState);
+  }
+
   return (
     <div className="wrapper login-page">
-      <form method="post">
+      <form onSubmit={onSubmit}>
         <div className="login">
           <legend className="title-login">Fazer login</legend>
-          <input type="text" placeholder="Nome da banca" name="banca" onChange={onChange} value={values.banca} />
-          <input type="password" placeholder="Senha" name="password" onChange={onChange} value={values.password} />
+          <input id="banca "type="text" placeholder="Nome da banca" name="banca" onChange={onChange} value={values.banca} />
+          <input id="password" type="password" placeholder="Senha" name="password" onChange={onChange} value={values.password} />
           <label className="remember">
             <input type="checkbox" name="remember" /> <p>Lembrar acesso</p>
           </label>
           <div className="button-login">
-            <Link to='/dashboard'>
+
               <button className="principal-button" type="submit">Acessar</button>
-            </Link>
+
           </div>
           <p>ou</p>
           <div className="button-contact">
