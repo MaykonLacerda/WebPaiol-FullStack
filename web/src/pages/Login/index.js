d18d21b4fc49caba46e6accbe953b6ea7b81703a
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-06-03 16:20:08 -0300

    routes private

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,42 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import api from '../../services/api';
 
-import PrincipalButton from '../../components/Buttons/PrincipalButton/PrincipalButton'
 import SecondButton from '../../components/Buttons/SecondButton/SecondButton'
 
 import './styles.css'
 
-export const Login = () => {
+function initialState() {
+  return { banca: '', password: '' };
+}
 
-  const teste = () => {
-    api.get('/login').then(response => {
-      const data = response.data;
-      console.log(data[0].name)
-    }).catch(err => {
-      if(err) {
-        console.log(err)
-      }
+export const Login = () => {
+  const [values, setValues] = useState(initialState);
+
+  function onChange(event) {
+    const { value, name } = event.target;
+
+    setValues({
+      ...values,
+      [name]: value
     })
   }
 
   return (
     <div className="wrapper login-page">
-      {teste()}
       <form method="post">
         <div className="login">
           <legend className="title-login">Fazer login</legend>
-          <input type="text" placeholder="Nome da banca" name="banca"></input>
-          <input type="password" placeholder="Senha" name="psw"></input>
+          <input type="text" placeholder="Nome da banca" name="banca" onChange={onChange} value={values.banca} />
+          <input type="password" placeholder="Senha" name="password" onChange={onChange} value={values.password} />
           <label className="remember">
-            <input type="checkbox" name="remember"/> <p>Lembrar acesso</p>
+            <input type="checkbox" name="remember" /> <p>Lembrar acesso</p>
           </label>
           <div className="button-login">
-            <PrincipalButton className="button-login" to="/home" text="Acessar"></PrincipalButton>
+            <Link to='/dashboard'>
+              <button className="principal-button" type="submit">Acessar</button>
+            </Link>
           </div>
           <p>ou</p>
           <div className="button-contact">
-            <SecondButton className="button-login" to="/home" text="Entrar em contato"></SecondButton>
+            <SecondButton className="button-login" to="/" text="Entrar em contato"></SecondButton>
           </div>
         </div>
       </form>
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import StoreProvider from './components/Store/Provider'
+import RoutesPrivate from './components/Routes/Private/Private'
+
 import Wellcome from './pages/Wellcome';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
 function Routes() {
   return (
-      <BrowserRouter>
-          <Route path="/" exact component={Wellcome} />
-          <Route path="/login" component={Login} />
-          <Route path="/dashboard" component={Dashboard} />
-      </BrowserRouter>
+      <StoreProvider>
+          <BrowserRouter>
+            <Route path="/" exact component={Wellcome} />
+            <Route path="/login" component={Login} />
+            <RoutesPrivate path="/dashboard" component={Dashboard} />
+        </BrowserRouter>
+      </StoreProvider>
   );
 }
 
